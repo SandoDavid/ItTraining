e4c7a2e81dd9450a7501d5b911cc9e993ffff13b
SandoDavid <davidsandoval@ginebro.cat>
2020-11-30 19:22:30 +0100

    no va

---
+++ js/preguntes.js
@@ -0,0 +1,10 @@
+let arrRespostes = [], arrRespostesCorrectes = [];
+arrRespostesCorrectes=[1,3,4,2];
+
+
+function pregunta1(){
+arrRespostes[1] = document.formulari.resposta1.value;
+
+alert("Has escollit resposta" + arrRespostes[1]);
+
+}
