d30a314e596a0a1cf152a7b3dc8045ab7495cf25
SandoDavid <davidsandoval@ginebro.cat>
2020-12-09 18:22:27 +0100

    Ja funciona, he modificat la variable

--- js/preguntes.js
+++ js/preguntes.js
@@ -1,43 +1,42 @@
 let arrRespostes = [], arrRespostesCorrectes = [];
-arrRespostesCorrectes=[1,3,4,2];
+arrRespostesCorrectes = [1, 3, 4, 2];
 
 
-function pregunta1(objecte, pregNum){
-    
-    let obj;
+function pregunta1(objecte, pregNum) {
+
+    let resposta;
+    let pregunta;
     console.log("pos = 0 " + objecte.form[0].checked);
     console.log("pos = 1 " + objecte.form[1].checked);
     console.log("pos = 2 " + objecte.form[2].checked);
     console.log("pos = 3 " + objecte.form[3].checked);
-    let resposta;
-    for (let i=0; i<4; i++ ){
+    for (let i = 0; i < 4; i++) {
         if (objecte.form[i].checked) {
-            resposta = i +1;
+            resposta = i + 1;
         }
     }
-arrRespostes[pregNum] = resposta;
+    arrRespostes[pregNum] = resposta;
 
-alert("Has escollit resposta" + arrRespostes[pregNum]);
-debugger;
-switch(pregNum) {
-    case 0:
-        
-        obj = document.getElementById("pregunta1");
+    alert("Has escollit resposta" + arrRespostes[pregNum]);
+    debugger;
+    switch (pregNum) {
+        case 0:
 
-        obj.classList.remove("elementVisible");
-        obj.classList.add("elementOcult");
+            pregunta = document.getElementById("pregunta1");
+            pregunta.classList.remove("elementVisible");
+            pregunta.classList.add("elementOcult");
 
-        
-        obj = document.getElementById("pregunta2");
-        obj.classList.add("elementOcult");
-        
-        
-        obj.classList.remove("elementVisible");
+            pregunta = document.getElementById("pregunta2");
+            pregunta.classList.remove("elementVisible");
+            pregunta.classList.add("elementVisible");
+            break;
 
-}
-
+        case 1:
+            alert("Hola");
+            break;
 
 
+    }
 
 
 
